bfd491e06c394a34923952264922a4f950029a67
Hassaan Nadeem <hassaan.butt9999@gmail.com>
2022-05-03 22:13:08 +0500

    fixed API call

--- app/script.js
+++ app/script.js
@@ -1,9 +1,50 @@
 const switcher = document.getElementById("btn-click");
 
-switcher.onclick = function() {
-    alert("Hello! I am an alert box!!");
-    const ins = document.getElementById("text").value;
+switcher.onclick = async function() {
+console.log("Button Clicked");
 
+
+
+
+
+    //alert("Hello! I am an alert box!");
+    const ins = document.getElementById("text").value;
+    //console.log(ins);
+    const url = "http://23.99.194.19:5000/get-text?ins=" + ins; 
+    console.log(url);
+//     // fetch data as text from url and display in alert
+    fetch(url)
+    .then(response => response.text())
+    .then((response) => {
+        alert(ins + response);
+        console.log(ins + response);
+    })
+    .catch(err => console.log(err))
+}
+
+
+
+
+
+
+
+
+
+
+
+    // async function fetchText() {
+    //     // alert("Generating response...")
+    //     // alert("Inside func")
+    //     // let response = await fetch('http://gpt3app.eastus.cloudapp.azure.com:5000/get-text?ins=hoo');
+    //     // let data = await response.json();
+    //     let response = await fetch(url)
+    //     // alert("Response recevived")
+    //     let data = await response.json()
+    //     // alert(data.text)
+    //     //temp.innerHTML = data.text;
+    //     // alert(data.quote);
+    // }
+    // fetchText();
     // const url = 'http://23.99.194.19:5000/get-text?ins=' + ins; 
 
     // async function fetchText(){
@@ -12,7 +53,9 @@ switcher.onclick = function() {
     //     alert(data.text);
     // }
     // fetchText();
-    ins.innerHTML = "Hello World!";
-}
+    //ins.innerHTML = "Hello World!";
+//     alert(url.text)
+    
+// }
 
 // console.log(ins);
